c98d9f173ece900e2289b0dea39b75de2cf3863b
Forrest <forforr@gmail.com>
2020-02-04 14:43:48 -0800

    Updated gitignore to ignore geckodriver, also switched to geckodriver

--- src/reportscanner.py
+++ src/reportscanner.py
@@ -1,14 +1,10 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options  
+from selenium.webdriver.firefox.options import Options
 from bs4 import BeautifulSoup
-import os
 
-chrome_options = Options()  
-chrome_options.add_argument('--headless')
-
-if os.name == 'nt':
-	driver = webdriver.Chrome(executable_path='chromedriver.exe')
-
+options = Options()
+options.headless = True
+driver = webdriver.Firefox(options=options, executable_path="geckodriver-v0.26.0-linux64/geckodriver")
 driver.get('https://www.snow.com/mountainconditions/snowandweatherreports.aspx')
 
 soup = BeautifulSoup(driver.page_source, 'html.parser')
@@ -18,10 +14,9 @@ soup = BeautifulSoup(driver.page_source, 'html.parser')
 # The following is dependant on the "lmxl" library
 # soup = BeautifulSoup(url_get.content, 'lmxl')
 
-
-
 # print(soup.prettify())
-print(soup.findAll("ul", {"class": "snow_report__metrics col-xs-12"}))
+print(soup.findAll("h1", {"class": "hero__content__title reverse"}))
+# print(soup.findAll("ul", {"class": "snow_report__metrics col-xs-12"}))
 print(soup.findAll("h5", {"class": "snow_report__metrics__measurement c78__total1--v1"}))
 
 #snow_report_1 > div.snow_report__content.row > ul > li:nth-child(7) > div > h5
